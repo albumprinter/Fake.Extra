2bca45e215e1e0be81fd41dc382b46b49a35e4a2
Denys Zhuravel <denys.zhuravel@albelli.com>
2018-07-02 16:24:25 +0200

    Add parameter store module

---
+++ ParameterStore.fs
@@ -0,0 +1,38 @@
+// In order for this module to work, the following dlls should be included in the main script:
+// #r "AWSSDK.Core.dll"
+// #r "AWSSDK.SimpleSystemsManagement.dll"
+
+namespace Albelli
+
+open Amazon.SimpleSystemsManagement
+
+    module ParameterStore =
+        let private parameterStoreParameters() = lazy (
+          let parameterStorePrefix = "/build/variables"
+          let parameterStorePrefixTrailingSlash = parameterStorePrefix + "/"
+          use client = new AmazonSimpleSystemsManagementClient()
+
+          Model.GetParametersByPathRequest (
+                      WithDecryption = true,
+                      Path = parameterStorePrefix,
+                      MaxResults = 10, // should be more than enough for this script
+                      Recursive = true)
+          |> client.GetParametersByPath
+          |> fun x-> x.Parameters
+          |> Seq.map (fun p -> p.Name.Substring(parameterStorePrefixTrailingSlash.Length), p.Value)
+          |> Map.ofSeq
+        )
+
+        let parameterStoreVarOrNone var = 
+            parameterStoreParameters().Value
+            |> Map.tryFind var
+
+        let anyVarOrNone var = 
+            match environVarOrNone var with
+            | None _ -> parameterStoreVarOrNone var
+            | x -> x
+
+        let anyVarOrFail var = 
+            match anyVarOrNone var with
+            | None _ -> failwithf "Can't find %s variable anywhere" var
+            | Some x -> x
