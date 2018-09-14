173225de370606b35b3b8d4dba423a824b7a44f0
Denys Zhuravel <denys.zhuravel@albelli.com>
2018-09-14 14:06:41 +0200

    Amazon.Lambda.Tools is now only supported as global tools

--- DotNet.fs
+++ DotNet.fs
@@ -5,21 +5,30 @@ open Fake
     module DotNet =
         let projectHasLambdaTools (projectPath : string) : bool =
             System.IO.File.ReadAllText projectPath
-            |> fun x -> x.Contains "Amazon.Lambda.Tools"
+            |> fun x -> x.Contains "<AWSProjectType>Lambda</AWSProjectType>"
 
-        let packageProjectAsLambda lambdaFramework outputFolder projectPath =
+        let packageProjectAsLambdaUsingGlobalTools lambdaFramework outputFolder projectPath =
             let projectDirectory = System.IO.Path.GetDirectoryName projectPath
             let projectName = System.IO.Path.GetFileName projectDirectory
             let outputFile = outputFolder </> (projectName + ".zip") |> System.IO.Path.GetFullPath
-            sprintf "lambda package --output-package %s --configuration Release --framework %s" outputFile lambdaFramework
-            |> DotNetCli.RunCommand (fun o -> { o with WorkingDir = projectDirectory } )
+            sprintf "lambda package --output-package %s --configuration Release --framework %s --project-location %s" outputFile lambdaFramework projectDirectory
+            |> DotNetCli.RunCommand id
 
-        let packageProjectAsLambdaDefaultFramework outputFolder projectPath = 
+        let getFrameworkFromProject projectPath = 
             let xPath = "/Project/PropertyGroup/TargetFramework/text()"
-            let framework 
-                = projectPath
+            projectPath
                   |> System.IO.File.ReadAllText
                   |> XMLDoc
                   |> XPathValue xPath []
-    
-            packageProjectAsLambda framework outputFolder projectPath
+                  
+        let packageProjectAsLambdaDefaultFrameworkUsingGlobalTools outputFolder projectPath = 
+            let framework = getFrameworkFromProject projectPath
+            packageProjectAsLambdaUsingGlobalTools framework outputFolder projectPath            
+
+        let installGlobalToolPackage package version =
+            sprintf "tool install -g %s --version %s" package version
+            |> DotNetCli.RunCommand id
+
+        let installToolPackage package version directory =
+            sprintf "tool install %s --version %s --tool-path %s" package version directory
+            |> DotNetCli.RunCommand id
