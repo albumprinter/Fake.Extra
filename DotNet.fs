aca2fbfb780cfe6bb05a06dc0204e1dbc7f3c999
kolbasik <sergii.kolbasin@albelli.com>
2020-11-10 13:33:56 +0100

    Mark properties explicitly with types for dotnet core 3.1

--- DotNet.fs
+++ DotNet.fs
@@ -4,7 +4,7 @@ open Fake
 
     module DotNet =
 
-        let private addToPath directory = 
+        let private addToPath (directory : string) : unit =
             let pathvar = System.Environment.GetEnvironmentVariable("PATH")
             let fullPath = directory |> System.IO.Path.GetFullPath
             let separator = if EnvironmentHelper.isLinux then ":" else ";";
@@ -16,33 +16,29 @@ open Fake
             System.IO.File.ReadAllText projectPath
             |> fun x -> x.Contains "<AWSProjectType>Lambda</AWSProjectType>"
 
-        let isEcsMainProject (projectPath : string) : bool =
-            System.IO.File.ReadAllText projectPath
-            |> fun x -> x.Contains "<PropertyGroup Label=\"EcsMainProject\" />"
-
-        let packageProjectAsLambdaUsingGlobalTools lambdaFramework outputFolder (projectPath : string) =
+        let packageProjectAsLambdaUsingGlobalTools (lambdaFramework : string) (outputFolder : string) (projectPath : string) : unit =
             let projectDirectory = System.IO.Path.GetDirectoryName projectPath
             let projectName = System.IO.Path.GetFileName projectDirectory
             let outputFile = outputFolder </> (projectName + ".zip") |> System.IO.Path.GetFullPath
             sprintf "lambda package --output-package %s --configuration Release --framework %s --project-location %s" outputFile lambdaFramework projectDirectory
             |> DotNetCli.RunCommand id
 
-        let private getFrameworkFromProject projectPath = 
+        let private getFrameworkFromProject (projectPath : string) : string =
             let xPath = "/Project/PropertyGroup/TargetFramework/text()"
             projectPath
                   |> System.IO.File.ReadAllText
                   |> XMLDoc
                   |> XPathValue xPath []
-                  
-        let packageProjectAsLambdaDefaultFrameworkUsingGlobalTools outputFolder projectPath = 
-            let framework = getFrameworkFromProject projectPath
-            packageProjectAsLambdaUsingGlobalTools framework outputFolder projectPath            
 
-        let installGlobalToolPackage package version =
+        let packageProjectAsLambdaDefaultFrameworkUsingGlobalTools (outputFolder : string) (projectPath : string) : unit =
+            let framework = getFrameworkFromProject projectPath
+            packageProjectAsLambdaUsingGlobalTools framework outputFolder projectPath
+
+        let installGlobalToolPackage (package : string) (version : string) : unit =
             sprintf "tool install -g %s --version %s" package version
             |> DotNetCli.RunCommand id
 
-        let installToolPackage package version directory =
+        let installToolPackage (package : string) (version : string) (directory : string) : unit =
             sprintf "tool install %s --version %s --tool-path %s" package version directory
             |> DotNetCli.RunCommand id
             addToPath(directory)
